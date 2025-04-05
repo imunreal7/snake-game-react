65ec6de0a344b718216c047b0cd59e93d06d373d
imunreal7 <dubeyaman174@gmail.com>
2025-04-05 15:33:04 +0530

    initial commit

---
+++ src/components/SnakeHeader/SnakeHeader.js
@@ -0,0 +1,13 @@
+import "../../App.css";
+import "../../utils.css";
+
+function SnakeHeader() {
+    return (
+        <div className="flex">
+            <div className="title">SNAKE GAME</div>
+        </div>
+    );
+}
+
+export default SnakeHeader;
+
